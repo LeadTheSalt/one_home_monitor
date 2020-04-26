5b32ebc870a6544561606a39ecbbd978ca46bec0
LeadtheSalt <leadthesalt.soc@gmail.com>
2020-04-26 11:35:31 +0200

    first commit, workingish, nor packaged well

---
+++ README.md
@@ -0,0 +1,52 @@
+[![Build Status](https://travis-ci.org/LeadTheSalt/one_home_monitor.svg?branch=master)](https://travis-ci.org/LeadTheSalt/one_home_monitor)
+
+# One Home Monitor 
+This peace of sofware aims to read data stored by onehomesensor projet (on my github page). It is web server writen thanks in GO, it provides a web page with graphs and a backend to get information in MongoDB storage. 
+
+## Usage 
+```
+onehomeminitor [OPTIONS]
+```
+Navigate to http://localhost:8080/
+
+Options are :
+  * conf_file : path to configuration file, mandatory for mongoDB connection
+  * bind : IP and port to bind on, default is ":8080"
+  * static_path : path to accès the static elements (home page and favicon)
+  * log_file : custom logging file, default is standart stdout.
+
+Backend part is accessible on url  http://localhost:8080/sensordata?f=[START_TIMESTAMP]&t=[END_TIMESTAMP]. It will send all reading between start and end timestamps. 
+
+## Integration / Instalation
+I advice to use Docker. Docker file is provided. TODO
+```
+docker build ...
+docker run ... 
+```
+
+Otherwise:
+```
+git clone 
+cd onehomemonitor
+go build onemonitor 
+./onemonitor
+```
+
+## Configuration file 
+```
+[MongoDBAtlasConnection]
+username = # username set for MongoDB
+password = # password set for MongoDB
+clusterfqdn = # fqdn to MongoDB server 
+
+```
+
+## Extra Documenation/Help
+https://www.alexedwards.net/blog/serving-static-sites-with-go
+https://stackoverflow.com/questions/26152993/go-logger-to-print-timestamp
+http://decouvric.cluster013.ovh.net/golang/gestion-des-erreurs-en-go.html
+https://stackoverflow.com/questions/55352362/filter-in-golang-mongodb
+https://kb.objectrocket.com/mongo-db/how-to-construct-mongodb-queries-from-a-string-using-golang-551
+https://github.com/chartjs/Chart.js
+https://getuikit.com/docs/introduction
+https://www.ijsselmeervogels.nl/cd
---
+++ onehomemonitor.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"strconv"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/net/context"
+	"fmt"
+	"flag"
+	"io/ioutil"
+	"os"
+	"time"
+	"log"
+	"net/http"
+	"regexp"
+	"path/filepath"
+	"github.com/pelletier/go-toml"
+	"go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var bind string
+var static_path string 
+var log_file string
+type MongoDB_con_conf struct {
+    Username    string
+    Password    string
+    ClusterFQDN string
+}
+type Configuration struct {
+	MongoDBConnexionConfiguration	MongoDB_con_conf
+}
+var running_conf Configuration
+var conf_file string
+var logger *log.Logger
+
+func init(){
+	// Read running args 
+	flag.StringVar(&conf_file, "conf_file", "./onehomemonitor.toml", "Path to configuration file")
+	flag.StringVar(&bind, "bind", ":8080", "IP:Port to bind listen socket")
+	flag.StringVar(&static_path, "static_path", "./static", "Path to folder holding static files")
+	flag.StringVar(&log_file, "log_file", "os.stdout", "Path to logging file")
+	flag.Parse()
+
+	// Prépare logging file
+	if log_file == "os.stdout"{
+		logger = log.New(os.Stdout, "", log.Lshortfile)
+	}else{
+		f, err := os.OpenFile(log_file,os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		logger = log.New(f, "", log.Lshortfile)
+	}
+
+	// read configuration file 
+	conf_file_data, err := ioutil.ReadFile(conf_file)
+	if err != nil {
+		log.Fatal(err)
+	} 
+	toml.Unmarshal(conf_file_data, &running_conf)
+}
+
+func loginfo(info string){
+	logger.SetPrefix(time.Now().Format("2020-04-24 19:18:17") + " [INFO] ")
+    logger.Print(info)
+}
+
+
+func mainpageHandler(w http.ResponseWriter, req *http.Request){
+	loginfo(fmt.Sprintf("Query on URL : %s", req.URL.Path))
+	var favicon_target= regexp.MustCompile("/favicon.*")
+	var is_favicon_target= favicon_target.MatchString(req.URL.Path)
+	if is_favicon_target {
+		http.ServeFile(w, req, filepath.Join(static_path, "favicon.ico"))
+	} else {
+		http.ServeFile(w, req, filepath.Join(static_path, "home.html"))
+	}
+}
+
+
+func dataHandler(w http.ResponseWriter, req *http.Request){
+	loginfo(fmt.Sprintf("Query on URL : %s with query %s", req.URL.Path, req.URL.RawQuery))
+
+	type Reading struct {
+		Ti string
+		Te string
+		Pr string
+		Hu string
+	}
+	type OutReading struct {
+		Te string
+		Pr string
+		Hu string
+	}
+	
+	// Prepare filter arguments 
+	// f : from, t : to and l : limite 
+	url_query := req.URL.Query()
+	var query_filter bson.M
+	if url_query.Get("f") != "" && url_query.Get("t") != ""{
+		query_filter = bson.M{ 
+			"ti": bson.M{
+				"$gte": url_query.Get("f"),
+				"$lte": url_query.Get("t"),
+			},
+		}
+	} else if url_query.Get("f") != "" {
+		query_filter = bson.M{ 
+			"ti": bson.M{
+				"$gte": url_query.Get("f"),
+			},
+		}
+	} else {
+		query_filter = bson.M{}
+	}
+	
+	// Prepare db connection
+	var res = map[string]map[int]OutReading{}
+	mongo_con_url := fmt.Sprintf("mongodb+srv://%s:%s@%s/test?retryWrites=true&w=majority",
+	running_conf.MongoDBConnexionConfiguration.Username,
+	running_conf.MongoDBConnexionConfiguration.Password,
+	running_conf.MongoDBConnexionConfiguration.ClusterFQDN)
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongo_con_url))
+	if err != nil {http.Error(w, err.Error(), 500)}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil { http.Error(w, err.Error(), 500)	}
+
+	// Register sensors readings
+	db := client.Database("onehomesensor") //database name hardcoded, as in collecting project
+	col_names , err := db.ListCollectionNames(context.Background(), bson.D{})
+	if err != nil { http.Error(w, err.Error(), 500) }
+	for _, v := range col_names {
+		res[v] = map[int]OutReading{}
+		coll := db.Collection(v)
+		cur, err := coll.Find(ctx, query_filter )
+		if err != nil { http.Error(w, err.Error(), 500) }
+		defer cur.Close(ctx)
+		//readings = make(map(int)interface{})
+		for cur.Next(ctx) {		
+   			var r Reading 
+   			err := cur.Decode(&r)
+   			if err != nil { log.Fatal(err) }
+			time, err := strconv.Atoi(r.Ti)
+			if err != nil { http.Error(w, err.Error(), 500) }
+			res[v][time] = OutReading{r.Te,r.Pr,r.Hu} 
+		}
+	}
+	json.NewEncoder(w).Encode(res)
+}
+
+
+func main() {
+	loginfo(fmt.Sprintf("Stating server on : %s", bind))
+	http.HandleFunc("/sensordata",dataHandler)
+	http.HandleFunc("/", mainpageHandler) 
+	log.Fatal(http.ListenAndServe(bind, nil))
+}
